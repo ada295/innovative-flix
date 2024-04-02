2aafa71cec06af666d4632105969ff9bdcbe82c7
Adrianna Zajac <adrianna.maria.zajac@gmail.com>
2024-04-02 15:51:59 +0200

    Added categories lwc.

--- force-app/main/default/lwc/categories/categories.js
+++ force-app/main/default/lwc/categories/categories.js
@@ -1,3 +1,16 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getAllCategories from '@salesforce/apex/CategoryController.getAllCategories';
 
-export default class Categories extends LightningElement {}
+
+export default class Categories extends LightningElement {
+    categories = [];
+
+    @wire(getAllCategories)
+    allCategories({ error, data }) {
+        if (data) {
+            this.categories = data;
+        } else if (error) {
+            console.error('Error loading categories:', error);
+        }
+    }
+}
