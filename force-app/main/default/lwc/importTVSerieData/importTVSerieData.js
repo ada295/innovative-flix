e4b22e6c07830d9679132eb7dfb4f46856f7213a
Adrianna Zajac <adrianna.maria.zajac@gmail.com>
2024-04-07 12:10:28 +0200

    Improved data importing

--- force-app/main/default/lwc/importTVSerieData/importTVSerieData.js
+++ force-app/main/default/lwc/importTVSerieData/importTVSerieData.js
@@ -1,22 +1,39 @@
 import { LightningElement, track } from 'lwc';
-import importTvSerie from '@salesforce/apex/DataImportController.importTvSerie';
+import importTvSerieById from '@salesforce/apex/DataImportController.importTvSerieById';
+import search from '@salesforce/apex/DataImportController.search';
 
 
 export default class ImportTVSerieData extends LightningElement {
     @track tvSerieName = '';
     @track showMessage = false;
+    @track error = '';
+    tvSeriesFromSearch = [];
 
     handleInputChange(event) {
         this.tvSerieName = event.target.value;
     }
 
-    importData() {
-        importTvSerie({ tvSerieName: this.tvSerieName })
+    search() {
+        search({ tvSerieName: this.tvSerieName })
+            .then(result => {
+                this.tvSeriesFromSearch = result;
+                console.log(result);
+            })
+            .catch(exception => {
+                this.error = exception.body.pageErrors[0].message;
+            });
+    }
+
+    importData(event) {
+        let tv = this.tvSeriesFromSearch[event.currentTarget.dataset.index];
+        //only id is sent beacuse salesforce does not support SIMPLE DTO as input....
+        importTvSerieById({ id: tv.id})
             .then(result => {
                 this.showMessage = true;
             })
-            .catch(error => {
-                console.error(error);
+            .catch(exception => {
+                // console.log(exception.body.pageErrors[0].message);
+                this.error = exception.body.pageErrors[0].message;
             });
     }
 }
--- force-app/main/default/triggers/TVSerieTrigger.trigger
+++ force-app/main/default/triggers/TVSerieTrigger.trigger
@@ -1,3 +1,7 @@
-trigger TVSerieTrigger on Tv_Serie__c (before insert) {
-
+trigger TVSerieTrigger on Tv_Serie__c (before insert, before update) {
+    if(Trigger.isBefore){
+        if (Trigger.isInsert || Trigger.isUpdate) {
+            TVSeriesHandler.checkUniqueTVSeriesName(Trigger.new);
+        }
+    }
 }
